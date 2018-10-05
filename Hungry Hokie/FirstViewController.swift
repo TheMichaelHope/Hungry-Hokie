6e6550916a15af638b50c55f5504063df48b2a22
TheMichaelHope <mh6698@vt.edu>
2018-10-05 14:44:57 -0400

    Initial Commit

---
+++ Hungry Hokie/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Hungry Hokie
+//
+//  Created by MH on 10/5/18.
+//  Copyright © 2018 Para. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
